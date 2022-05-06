757d54064b9ad067aaa403afc494972afec6e6c2
Andrea Guidotti <andrea19912014@gmail.com>
2022-05-06 18:02:19 +0200

    click apertura e chiusura

--- main.js
+++ main.js
@@ -1,19 +1,39 @@
 // se clicco su hamburger menu si aprirà il menu a tendina
 const fas = document.querySelector('.fas');
 
+
+// con questo apro il menu a tendina
 fas.addEventListener('click',
 
     function() {
         
         let open = document.querySelector('.hamburger-menu');
 
-        if (open === '.fas') {
-            open.style.display = 'none';
-            console.log(open);
-        } else {
-            open.style.display = 'block';
-            console.log(open);
-        }
+        open.style.display = ('block');
+
+        // if (open === '.fas') {
+        //     open.style.display = 'none';
+        //     console.log(open);
+        // } else { 
+        //     open === '.close';
+        //     open.style.display = 'block';
+        //     console.log(open);
+        // }
+    }
+
+);
+
+// se clicco di nuovo il menu a tendina si chiuderà
+const close = document.querySelector('.close');
+
+// con questo chiudo il menu a tendina
+close.addEventListener('click',
+
+    function() {
+
+        let close = document.querySelector('.hamburger-menu');
+
+        close.style.display = ('none');
     }
 
 );
